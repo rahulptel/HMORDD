9499f146a5f06f4fa03212262ea491eac75bb875
rahulptel <rahul.polymtl@gmail.com>
2025-12-04 00:53:39 -0500

    Generate binary for biobjective setpacking

--- src/cpp/setpacking/makelibcmd.sh
+++ src/cpp/setpacking/makelibcmd.sh
@@ -15,7 +15,7 @@ ROOT_DIR="$(realpath "${SCRIPT_DIR}/../../..")"
 DEST_DIR="${ROOT_DIR}/resources/bin/setpacking"
 mkdir -p "${DEST_DIR}"
 
-for i in {3..7}; do
+for i in {2..7}; do
 	sed -i "s/PYBIND11_MODULE(libsetpackingenv, m)/PYBIND11_MODULE(libsetpackingenvo$i, m)/g" libsetpackingenv.cpp
 	
 	makelib=$(echo "g++ -m64 -O3 -DIL_STD -DNOBJS=$i -Wall -shared -std=c++17 -fPIC -I./include $(python3 -m pybind11 --includes) libsetpackingenv.cpp setpackingenv.cpp ../common/bdd/*.cpp ../common/util/*.cpp ./bdd/*.cpp ./instances/*.cpp $(python3-config --ldflags) -lm -ldl -pthread -o libsetpackingenvo$i${EXT_SUFFIX}")
