669b3a87575026a65a8ab866f0b2c4c415de45c4
rahulptel <rahul.polymtl@gmail.com>
2026-05-13 01:34:19 -0400

    chore(setpacking): restrict NSGA-II postprocess variants

--- src/py/hmordd/setpacking/postprocess.py
+++ src/py/hmordd/setpacking/postprocess.py
@@ -15,7 +15,6 @@ EXPERIMENT_SPECS = (
         nsga2_variants=(
             nsga2_variant("pop100_time1", pop_size=100, run_time=1),
             nsga2_variant("pop500_time1", pop_size=500, run_time=1),
-            nsga2_variant("pop250_time1", pop_size=250, run_time=1),
         ),
     ),
     ExperimentSpec(
@@ -25,7 +24,6 @@ EXPERIMENT_SPECS = (
         nsga2_variants=(
             nsga2_variant("pop100_time1", pop_size=100, run_time=1),
             nsga2_variant("pop500_time1", pop_size=500, run_time=1),
-            nsga2_variant("pop1100_time1", pop_size=1100, run_time=1),
         ),
     ),
     ExperimentSpec(
@@ -35,7 +33,6 @@ EXPERIMENT_SPECS = (
         nsga2_variants=(
             nsga2_variant("pop100_time1", pop_size=100, run_time=1),
             nsga2_variant("pop500_time1", pop_size=500, run_time=1),
-            nsga2_variant("pop4600_time1", pop_size=4600, run_time=1),
         ),
     ),
     ExperimentSpec(
@@ -45,7 +42,6 @@ EXPERIMENT_SPECS = (
         nsga2_variants=(
             nsga2_variant("pop100_time2", pop_size=100, run_time=2),
             nsga2_variant("pop500_time2", pop_size=500, run_time=2),
-            nsga2_variant("pop9000_time2", pop_size=9000, run_time=2),
         ),
     ),
     ExperimentSpec(
@@ -55,7 +51,6 @@ EXPERIMENT_SPECS = (
         nsga2_variants=(
             nsga2_variant("pop100_time13", pop_size=100, run_time=13),
             nsga2_variant("pop500_time13", pop_size=500, run_time=13),
-            nsga2_variant("pop25000_time13", pop_size=25000, run_time=13),
         ),
     ),
     ExperimentSpec(
@@ -65,7 +60,6 @@ EXPERIMENT_SPECS = (
         nsga2_variants=(
             nsga2_variant("pop100_time1", pop_size=100, run_time=1),
             nsga2_variant("pop500_time1", pop_size=500, run_time=1),
-            nsga2_variant("pop800_time1", pop_size=800, run_time=1),
         ),
     ),
     ExperimentSpec(
@@ -75,7 +69,6 @@ EXPERIMENT_SPECS = (
         nsga2_variants=(
             nsga2_variant("pop100_time7", pop_size=100, run_time=7),
             nsga2_variant("pop500_time7", pop_size=500, run_time=7),
-            nsga2_variant("pop6100_time7", pop_size=6100, run_time=7),
         ),
     ),
     ExperimentSpec(
@@ -85,7 +78,6 @@ EXPERIMENT_SPECS = (
         nsga2_variants=(
             nsga2_variant("pop100_time77", pop_size=100, run_time=77),
             nsga2_variant("pop500_time77", pop_size=500, run_time=77),
-            nsga2_variant("pop30000_time77", pop_size=30000, run_time=77),
         ),
     ),
     ExperimentSpec(
@@ -95,7 +87,6 @@ EXPERIMENT_SPECS = (
         nsga2_variants=(
             nsga2_variant("pop100_time182", pop_size=100, run_time=182),
             nsga2_variant("pop500_time182", pop_size=500, run_time=182),
-            nsga2_variant("pop60000_time182", pop_size=60000, run_time=182),
         ),
     ),
     ExperimentSpec(
@@ -105,7 +96,6 @@ EXPERIMENT_SPECS = (
         nsga2_variants=(
             nsga2_variant("pop100_time313", pop_size=100, run_time=313),
             nsga2_variant("pop500_time313", pop_size=500, run_time=313),
-            nsga2_variant("pop10400_time313", pop_size=10400, run_time=313),
         ),
     ),
 )
